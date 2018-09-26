7514fefe42c92fcce24caab3a5354025e5ea8507
arvention <amc.antioquia@yahoo.com.ph>
2018-09-26 17:00:53 +0800

    corrected indentation and spacing, and changed format of file name for saving model

--- solver.py
+++ solver.py
@@ -16,6 +16,10 @@ class Solver(object):
     DEFAULTS = {}
 
     def __init__(self, version, data_loader, config):
+        """
+        Initializes a Solver object
+        """
+
         # data loader
         self.__dict__.update(Solver.DEFAULTS, **config)
         self.version = version
@@ -23,42 +27,44 @@ class Solver(object):
 
         self.build_model()
 
-        # build tensorboard if use
-        # if self.use_tensorboard:
-        #    self.build_tensorboard()
+        # TODO: build tensorboard
 
-        # start with a trained model
+        # start with a pre-trained model
         if self.pretrained_model:
             self.load_pretrained_model()
 
     def build_model(self):
+        """
+        Instantiates the model, loss criterion, and optimizer
+        """
 
-        # instantiate ZFNet model
+        # instantiate model
         self.model = ZFNet(self.input_channels, self.class_count)
 
         # instantiate loss criterion
         self.criterion = nn.CrossEntropyLoss()
 
         # instantiate optimizer
-        self.optimizer = optim.SGD(
-            self.model.parameters(),
-            lr=self.lr,
-            momentum=self.momentum  # ,
-            # weight_decay=1e-4
-        )
+        self.optimizer = optim.SGD(self.model.parameters(),
+                                   lr=self.lr,
+                                   momentum=self.momentum)
 
         self.scheduler = lr_scheduler.ReduceLROnPlateau(self.optimizer,
                                                         patience=5,
                                                         verbose=True)
 
-        # print networks
+        # print network
         self.print_network(self.model, 'ZFNet')
 
+        # use gpu if enabled
         if torch.cuda.is_available() and self.use_gpu:
             self.model.cuda()
             self.criterion.cuda()
 
     def print_network(self, model, name):
+        """
+        Prints the structure of the network and the total number of parameters
+        """
         num_params = 0
         for p in model.parameters():
             num_params += p.numel()
@@ -67,11 +73,17 @@ class Solver(object):
         print("The number of parameters: {}".format(num_params))
 
     def load_pretrained_model(self):
+        """
+        loads a pre-trained model from a .pth file
+        """
         self.model.load_state_dict(torch.load(os.path.join(
             self.model_save_path, '{}.pth'.format(self.pretrained_model))))
-        print('loaded trained model (step: {})'.format(self.pretrained_model))
+        print('loaded trained model ver {}'.format(self.pretrained_model))
 
     def print_loss_log(self, start_time, iters_per_epoch, e, i, loss):
+        """
+        Prints the loss and elapsed time for each epoch
+        """
         total_iter = self.num_epochs * iters_per_epoch
         cur_iter = e * iters_per_epoch + i
 
@@ -83,30 +95,34 @@ class Solver(object):
         total_time = str(datetime.timedelta(seconds=total_time))
         elapsed = str(datetime.timedelta(seconds=elapsed))
 
-        log = "Elapsed {}/{} -- {} , Epoch [{}/{}], Iter [{}/{}], " \
-              "loss: {:.4f}".format(
-               elapsed,
-               epoch_time,
-               total_time,
-               e + 1,
-               self.num_epochs,
-               i + 1,
-               iters_per_epoch,
-               loss
-               )
+        log = "Elapsed {}/{} -- {}, Epoch [{}/{}], Iter [{}/{}], " \
+              "loss: {:.4f}".format(elapsed,
+                                    epoch_time,
+                                    total_time,
+                                    e + 1,
+                                    self.num_epochs,
+                                    i + 1,
+                                    iters_per_epoch,
+                                    loss)
 
-        # !!!TODO!!! ADD TENSORBOARD
+        # TODO: add tensorboard
 
         print(log)
 
     def save_model(self, e, i):
+        """
+        Saves a model per e epoch and i iteration
+        """
         path = os.path.join(
             self.model_save_path,
-            '{}_{}_{}.pth'.format(self.version, e + 1, i + 1)
+            '/{}/{}--{}.pth'.format(self.version, e + 1, i + 1)
         )
         torch.save(self.model.state_dict(), path)
 
     def model_step(self, images, labels):
+        """
+        A step for each iteration
+        """
 
         # set model in training mode
         self.model.train()
@@ -129,26 +145,25 @@ class Solver(object):
         return loss
 
     def train(self):
+        """
+        Training process
+        """
         self.losses = []
         self.top_1_acc = []
         self.top_5_acc = []
 
         iters_per_epoch = len(self.data_loader)
 
-        # start with trained model if exists
+        # start with a trained model if exists
         if self.pretrained_model:
-            start = int(self.pretrained_model.split('_')[0])
+            start = int(self.pretrained_model.split('--')[0])
         else:
             start = 0
 
         # start training
-        iter_ctr = 0
         start_time = time.time()
         for e in range(start, self.num_epochs):
             for i, (images, labels) in enumerate(tqdm(self.data_loader)):
-                iter_ctr += 1
-                start = time.time()
-
                 images = to_var(images, self.use_gpu)
                 labels = to_var(labels, self.use_gpu)
 
@@ -187,35 +202,45 @@ class Solver(object):
             print(e, '{:.4f}'.format(acc))
 
     def eval(self, data_loader):
+        """
+        Returns the count of top 1 and top 5 predictions
+        """
 
+        # set the model to eval mode
         self.model.eval()
 
         top_1_correct = 0
         top_5_correct = 0
         total = 0
-        for images, labels in data_loader:
 
-            images = to_var(images, self.use_gpu)
-            labels = to_var(labels, self.use_gpu)
+        with torch.no_grad():
+            for images, labels in data_loader:
 
-            output = self.model(images)
-            total += labels.size()[0]
+                images = to_var(images, self.use_gpu)
+                labels = to_var(labels, self.use_gpu)
 
-            # top 1
-            # get the max for each instance in the batch
-            _, top_1_output = torch.max(output.data, dim=1)
+                output = self.model(images)
+                total += labels.size()[0]
 
-            top_1_correct += torch.sum(torch.eq(labels.squeeze(),
-                                                top_1_output))
+                # top 1
+                # get the max for each instance in the batch
+                _, top_1_output = torch.max(output.data, dim=1)
 
-            # top 5
-            _, top_5_output = torch.topk(output.data, k=5, dim=1)
-            for i, label in enumerate(labels):
-                if label in top_5_output[i]:
-                    top_5_correct += 1
+                top_1_correct += torch.sum(torch.eq(labels.squeeze(),
+                                                    top_1_output))
+
+                # top 5
+                _, top_5_output = torch.topk(output.data, k=5, dim=1)
+                for i, label in enumerate(labels):
+                    if label in top_5_output[i]:
+                        top_5_correct += 1
+
         return top_1_correct.item(), top_5_correct, total
 
     def train_evaluate(self, e):
+        """
+        Evaluates the performance of the model using the train dataset
+        """
         top_1_correct, top_5_correct, total = self.eval(self.data_loader)
         log = "Epoch [{}/{}]--top_1_acc: {:.4f}--top_5_acc: {:.4f}".format(
             e + 1,
@@ -227,6 +252,9 @@ class Solver(object):
         return top_1_correct / total, top_5_correct / total
 
     def test(self):
+        """
+        Evaluates the performance of the model using the test dataset
+        """
         top_1_correct, top_5_correct, total = self.eval(self.data_loader)
         log = "top_1_acc: {:.4f}--top_5_acc: {:.4f}".format(
             top_1_correct / total,
